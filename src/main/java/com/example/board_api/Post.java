b1d18b115fb8460294d7241be5a0b502faab1f2e
Choi-Moori <anflwkd@gmail.com>
2024-07-08 10:32:40 +0900

    asdfa

---
+++ src/main/java/com/example/board_api/Post.java
@@ -0,0 +1,33 @@
+package com.example.board_api;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Post {
+    static Long staticId = 1L;
+    static List<Post> posts = new ArrayList<>();
+    private Long id;
+    private String title;
+    private String contents;
+
+    Post(String title, String contents){
+        this.id = staticId++;
+        this.title = title;
+        this.contents  = contents;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+}
